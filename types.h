fc785a532e2e013d123050842eed5c8c7a8fb7a2
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-06 21:47:45 +0200

    ADDED: type definitions
    Will be used eg. `int32_t` instead of `int`. Will be much easier to perform
    some fixes in one file if there will be need to add 32bit support.

--- types.h
+++ types.h
@@ -1,6 +1,17 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-// 
+#define DEBUGMODE 1
+
+#ifndef NULL
+#define NULL (void*)0
+#endif
+
+typedef short int int16_t;
+typedef unsigned short int uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef long int int64_t;
+typedef unsigned long int uint64_t;
 
 #endif
