52fd0a8221f9addbbb5f5d29902b900468c4c690
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-08 19:07:40 +0200

    reading vertices and triangles from *.brp files

--- src/scene.c
+++ src/scene.c
@@ -1,1 +1,95 @@
 #include "scene.h"
+#include "error.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+SCN_Scene* scn_scene_load(const char *filename) {
+    char c;
+    int32_t i, _i, j, k, len, vcount=-1, tcount=-1;
+    FILE *fd=NULL;
+    char *buffer=NULL;
+    SCN_Scene *res=NULL;
+
+    fd=fopen(filename, "r");
+    if (!fd) {
+        errno = E_IO;
+        goto cleanup;
+    }
+    
+    uint16_t buf_size=1024;
+    buffer = malloc(buf_size);
+    if (!buffer) {
+        errno = E_MEMORY;
+        goto cleanup;
+    }
+    
+    while(!feof(fd)) {
+        if(!fgets(buffer, buf_size, fd))
+            break;
+        len = strlen(buffer);
+        for(i=0; i<len; i++) {
+            c = buffer[i];
+            if(c!=' ' && c!='\t' && c!='\n' && c!='\r')
+                break;  //other character than white one
+        }
+        if(i == len)
+            continue;  // white chars only found
+        if(strstr(buffer, "//"))
+            continue;  //comment found
+
+        /* reading number of vertices */
+        if(vcount == -1) {
+            sscanf(buffer, "%d", &vcount);  //read number of vertices
+            res = malloc(sizeof(SCN_Scene));  //create SCN_Scene object
+            if (!res) {
+                errno = E_MEMORY;
+                goto cleanup;
+            }
+            i = 0;
+            res->vsize = vcount;
+            res->v = malloc(vcount*sizeof(SCN_Vertex));  //create array of vertices
+            if (!res->v) {
+                free(res);  //unable to allocate memory for vertices - NULL must be returned
+                res = NULL;
+                errno = E_MEMORY;
+                goto cleanup;
+            }
+
+        /* reading vertices into array */
+        } else if (vcount > 0) {
+            sscanf(buffer, "%f %f %f", &res->v[i].x, &res->v[i].y, &res->v[i].z);
+            i++; vcount--;
+
+        /* reading number of triangles */
+        } else if (tcount == -1) {
+            sscanf(buffer, "%d", &tcount);
+            i = 0;
+            res->tsize = tcount;
+            res->t = malloc(tcount*sizeof(SCN_Triangle));  //create array of triangles
+            if (!res->t) {
+                free(res);
+                res = NULL;
+                errno = E_MEMORY;
+                goto cleanup;
+            }
+
+        /* reading triangles into array */
+        } else if (tcount > 0) {
+            sscanf(buffer, "%d %d %d", &_i, &j, &k);
+            res->t[i].i = &res->v[_i];  //assign address of correct vertex
+            res->t[i].j = &res->v[j];
+            res->t[i].k = &res->v[k];
+            i++; tcount--;
+        }
+    }
+
+    cleanup:
+        if(fd)
+            fclose(fd);
+        if(buffer)
+            free(buffer);
+
+    return res;
+}
--- src/scene.h
+++ src/scene.h
@@ -1,4 +1,39 @@
 #ifndef __SCENE_H
 #define __SCENE_H
 
+#include "types.h"
+
+//// STRUCTURES ///////////////////////////////////////////////
+
+typedef struct _SCN_Vertex {
+    float x, y, z;  //vertex coords
+} SCN_Vertex;
+
+typedef struct _SCN_Triangle {
+    SCN_Vertex *i, *j, *k;  //pointers to triangle's vertices
+} SCN_Triangle;
+
+typedef struct _SCN_Scene {
+    int32_t vsize;   //number of vertices in scene
+    int32_t tsize;   //number of triangles in scene
+    SCN_Vertex *v;   //array of vertices
+    SCN_Triangle *t; //array of triangles
+} SCN_Scene;
+
+typedef struct _SCN_Camera {
+} SCN_Camera;
+
+typedef struct _SCN_Lights {
+} SCN_Lights;
+
+typedef struct _SCN_SurfaceAttribs {
+} SCN_SurfaceAttribs;
+
+//// FUNCTIONS ////////////////////////////////////////////////
+
+/* Loads scene geometry description from given *.brp file.
+ 
+ @param: filename: absolute path to geometry file */
+SCN_Scene* scn_scene_load(const char *filename);
+
 #endif
