f64aa805164199704afb8ea6ad1a8144110e1961
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-08 16:15:48 +0200

    add: module for error management (error.c)

---
+++ src/error.c
@@ -0,0 +1,36 @@
+#include <errno.h>
+#include "types.h"
+#include "error.h"
+
+
+/* Textual description of errors. 
+ * TODO: add description once another error code is added */
+static struct ERR_Description errdesc[] = {
+    //common error codes
+    {E_IO,                  "unable to read and/or write to file"},
+    {E_MEMORY,              "not enough memory"},
+
+    //error codes used by imagelib.c
+    {E_INVALID_FILE,        "not a BMP file"},
+    {E_INVALID_FILE_FORMAT, "BMP format not supported"},
+    {E_INVALID_BPP,         "invalid number of bits per pixel"},
+
+    //no error, unexpected error (array end signature)
+    {0,                     "success"},
+    {-1,                    "unexpected error"}
+};
+
+
+char* err_desc() {
+    int16_t i=0;
+    while(1) {
+        if (errno == errdesc[i].eid) {
+            return errdesc[i].desc;
+        } else if (errdesc[i].eid == -1) {
+            return errdesc[i].desc;
+        }
+        i++;
+    }
+}
+
+
---
+++ src/error.h
@@ -0,0 +1,34 @@
+#ifndef __ERROR_H
+#define __ERROR_H
+
+#include <errno.h>
+
+//// STRUCTURES ///////////////////////////////////////////////
+
+/* Structure containing single errno -> errdesc mapping. */
+struct ERR_Description {
+    int32_t eid; 
+    char desc[256];
+};
+
+//// ENUMERATIONS /////////////////////////////////////////////
+
+/* List of imagelib's errors that may occur. 
+ * TODO: add more error codes here */
+typedef enum _ERR_Code {
+    /* common */
+    E_IO=1,         //unable to read or write from/to file
+    E_MEMORY,       //unable to allocate memory
+
+    /* used by: imagelib.c */
+    E_INVALID_FILE,         //trying to load file of invalid format
+    E_INVALID_FILE_FORMAT,  //image file format is not supported
+    E_INVALID_BPP           //bits per pixel argument is invalid (correct ones are: 1, 4, 8, 16, 24, 32)
+} ERR_Code;
+
+//// FUNCTIONS ////////////////////////////////////////////////
+
+/* Gets text description of currently set `errno` variable. */
+char* err_desc();
+
+#endif
