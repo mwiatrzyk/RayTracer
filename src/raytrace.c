a40b99eaa615c4136dcbaaa2047658c25c5ebe14
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-15 13:30:22 +0200

    add raytrace() function

--- src/raytrace.c
+++ src/raytrace.c
@@ -4,6 +4,35 @@
 
 //// HELPER FUNCTIONS /////////////////////////////////////////
 
+/* RayTracing algorithm implementation. 
+
+ @param: t: pointer to scene's triangle array 
+ @param: maxt: pointer to first element above scene's triangle array 
+ @param: o: ray origin point
+ @param: ray: ray vector (normalized) */
+static int32_t raytrace(SCN_Triangle *t, SCN_Triangle *maxt, SCN_Vertex *o, SCN_Vertex *ray) {
+    // find nearest triangle that intersects with given ray
+    SCN_Triangle *nearest=t;
+    float d, dmin, ray_dotp_n;
+    //float rx=ray->x, ry=ray->y, rz=ray->z;
+    //float nx, ny, nz;
+    while(t < maxt) {
+        ray_dotp_n = vec_dotproduct(ray, &t->n);
+        if(ray_dotp_n == 0.0f) {  // intersection point is somewhere in infinity (ray is parallel to triangle)
+            t++;
+            continue;
+        }
+        d = -(vec_dotproduct(o, &t->n) + t->d)/ray_dotp_n;
+        if(d <= 0.0f) {  // TODO: t <= 0.0f or t < 0.0f ?
+            t++;
+            continue;  // intersection point is "behind" current ray
+        }
+        //printf("%f\n", d);
+        t++;
+    }
+}
+
+
 
 //// INTERFACE FUNCTIONS //////////////////////////////////////
 
