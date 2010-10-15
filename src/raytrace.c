6d71df7bbc4b16e706e1edff336f1d868306159e
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-15 13:36:14 +0200

    add preprocess_scene() function
    Used to generate some intensively used coefficients before RayTracing algorithm
    starts.

--- src/raytrace.c
+++ src/raytrace.c
@@ -33,12 +33,41 @@ static int32_t raytrace(SCN_Triangle *t, SCN_Triangle *maxt, SCN_Vertex *o, SCN_
 }
 
 
+/* Performs scene preprocessing. Calculates all constant coefficients for each
+ * triangle in scene. */
+static SCN_Scene* preprocess_scene(SCN_Scene *scene, SCN_Camera *camera) {
+    SCN_Triangle *t=scene->t, *maxt=(SCN_Triangle*)(scene->t + scene->tsize);
+    printf("%ld\n", scene->tsize);
+    while(t < maxt) {
+        /* calculate normal vector and orient it towards observer */
+        SCN_Vertex ij=vec_make_vector(t->i, t->j), ik=vec_make_vector(t->i, t->k);
+        SCN_Vertex oi=vec_make_vector(&camera->ob, t->i);
+        SCN_Vertex xprod = vec_crossproduct(&ij, &ik);
+        oi = vec_normalize(&oi);  // vector from observer towards current triangle (normalized)
+        t->n = vec_normalize(&xprod);  // normal vector of current triangle (normalized)
+        if(vec_dotproduct(&oi, &t->n) < 0.0f) {
+            t->n = vec_mul(&t->n, -1.0f);  // point normal vector towards observer
+        }
+
+        /* calculate `d` parameter of triangle's plane equation: i*n+d=0 ->
+         * d=-i*n, where: i - one of triangle's vertices, n - normal vector */
+        t->d = -vec_dotproduct(t->i, &t->n);
+
+        t++;
+    }
+
+    return scene;
+}
+
 
 //// INTERFACE FUNCTIONS //////////////////////////////////////
 
 IML_Bitmap* rtr_execute(SCN_Scene *scene, SCN_Camera *camera) {
     float x, y, w=camera->sw, h=camera->sh;
     SCN_Vertex *a=&camera->ul, *b=&camera->ur, *c=&camera->bl, *o=&camera->ob;
+    
+    // preprocess scene (calculate all needed coefficients etc.)
+    preprocess_scene(scene, camera);
 
     // main loop
     for(y=0.5f; y<h; y+=1.0f) {
