a14ee94bdfa63c4a8b1de36b9d86e8e7f6b51092
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-16 21:16:37 +0200

    add type `bool`

--- src/types.h
+++ src/types.h
@@ -7,6 +7,11 @@
 #define NULL (void*)0
 #endif
 
+typedef enum _bool {
+    false = 0,
+    true = 1
+} bool;
+
 typedef short int int16_t;
 typedef unsigned short int uint16_t;
 typedef int int32_t;
