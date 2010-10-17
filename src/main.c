32fc471bd794197f34cfb3631b2d1a01b494f565
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-17 15:39:22 +0200

    main() with hardcoded paths

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <time.h>
+#include <errno.h>
+#include "imagelib.h"
+#include "scene.h"
+#include "rdtsc.h"
+#include "raytrace.h"
 
 int main(int argc, char* argv[]) {
+    SCN_Scene *scene=scn_scene_load("./scenes/pokoj/s2.brs");
+    scn_scene_load_lights(scene, "./scenes/pokoj/s2.lgt");
+    scn_scene_load_surface(scene, "./scenes/pokoj/s2.atr");
+    SCN_Camera *cam=scn_camera_load("./scenes/pokoj/s2.cam");
+    
+    clock_t start = clock();
+    IML_Bitmap *bmp=rtr_execute(scene, cam);
+    printf("Time taken: %f seconds\n", (double)(clock()-start)/CLOCKS_PER_SEC);
+
+    iml_bitmap_save(bmp, "./result.bmp", 24);
+    iml_bitmap_destroy(bmp);
+
     return 0;
 }
