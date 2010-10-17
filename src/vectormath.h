74e7f14bf1684eccfdf6e8632e46c2cfa9bf7c37
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-17 14:26:28 +0200

    using pointers to result containers instead of creating result objects

--- src/vectormath.h
+++ src/vectormath.h
@@ -4,73 +4,81 @@
 #include <math.h>
 #include "scene.h"
 
-/* Creates vector pointing from point `a` towards point `b`. */
-inline SCN_Vertex vec_make_vector(SCN_Vertex *a, SCN_Vertex *b) {
-    SCN_Vertex res = {
-        b->x - a->x,
-        b->y - a->y,
-        b->z - a->z
-    };
-    return res;
+/* Copies given vector `self` and stores its copy in vector pointed by `dest`.
+ * Returns `dest`. */
+inline SCN_Vertex* vec_vector_copy(SCN_Vertex *self, SCN_Vertex *dest) {
+    dest->x = self->x;
+    dest->y = self->y;
+    dest->z = self->z;
+    return dest;
 }
 
-/* Returns length of given vector. */
-inline float vec_length(SCN_Vertex *v) {
-    return sqrt(v->x*v->x + v->y*v->y + v->z*v->z);
+/* Subtracts vector `b` from vector `a` and stores result in vector `self`.
+ * When `a` and `b` are interpreted as points, not vectors, this function will
+ * calculate vector from point `b` towards point `a`.  Returns `self`. */
+inline SCN_Vertex* vec_vector_sub(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+    self->x = a->x - b->x;
+    self->y = a->y - b->y;
+    self->z = a->z - b->z;
+    return self;
 }
 
-/* Calculates Euclidean distance between given two vectors. */
-inline float vec_distance(SCN_Vertex *v1, SCN_Vertex *v2) {
-    float dx=v1->x-v2->x, dy=v1->y-v2->y, dz=v1->z-v2->z;
+/* Alias for `vec_vector_sub`. Used to calculate vector from point `a` towards
+ * point `b`. */
+inline SCN_Vertex* vec_vector_make(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+    return vec_vector_sub(self, b, a);
+}
+
+/* Returns length of given vector `self`. */
+inline float vec_vector_length(SCN_Vertex *self) {
+    return sqrt(self->x*self->x + self->y*self->y + self->z*self->z);
+}
+
+/* Calculates Euclidean distance between given vector `self` and vector `v`. */
+inline float vec_vector_distance(SCN_Vertex *self, SCN_Vertex *v) {
+    float dx=self->x-v->x, dy=self->y-v->y, dz=self->z-v->z;
     return sqrt(dx*dx + dy*dy + dz*dz);
 }
 
-/* Normalizes given vector and returns normalized vector. */
-inline SCN_Vertex vec_normalize(SCN_Vertex *v) {
-    float len=sqrt(v->x*v->x + v->y*v->y + v->z*v->z);
-    SCN_Vertex res = {
-        v->x / len,
-        v->y / len,
-        v->z / len
-    };
-    return res;
+/* Normalizes given vector "inplace". Returns `self`. */
+inline SCN_Vertex* vec_vector_normalize(SCN_Vertex *self) {
+    float len=sqrt(self->x*self->x + self->y*self->y + self->z*self->z);
+    self->x /= len;
+    self->y /= len;
+    self->z /= len;
+    return self;
 }
 
-/* Multiplicates given vector by given constant and returns resulting vector. */
-inline SCN_Vertex vec_mul(SCN_Vertex *v, float t) {
-    SCN_Vertex res = {
-        v->x * t,
-        v->y * t,
-        v->z * t
-    };
-    return res;
+/* Multiplicates vector `v` by scalar `t` and stores result in `self`. Returns
+ * `self`. */
+inline SCN_Vertex* vec_vector_mul(SCN_Vertex *self, SCN_Vertex *v, float t) {
+    self->x = v->x * t;
+    self->y = v->y * t;
+    self->z = v->z * t;
+    return self;
 }
 
-/* Calculates and returns vector that is sum of given two vectors. If first
- * vector is interpreted as point this function performs translation of point
- * `v1` by vector `v2`. */
-inline SCN_Vertex vec_sum(SCN_Vertex *v1, SCN_Vertex *v2) {
-    SCN_Vertex res = {
-        v1->x + v2->x,
-        v1->y + v2->y,
-        v1->z + v2->z
-    };
-    return res;
+/* Adds vector `b` to vector `a` and stores result in `self`. Returns `self`. */
+inline SCN_Vertex* vec_vector_add(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+    self->x = a->x + b->x;
+    self->y = a->y + b->y;
+    self->z = a->z + b->z;
+    return self;
 }
 
-/* Calculates crossproduct of two vectors. */
-inline SCN_Vertex vec_crossproduct(SCN_Vertex *v1, SCN_Vertex *v2) {
-    SCN_Vertex res = {
-        v1->y*v2->z - v1->z*v2->y,
-        v1->z*v2->x - v1->x*v2->z,
-        v1->x*v2->y - v1->y*v2->x
-    };
-    return res;
+/* Calculates cross product of vectors `a` and `b` and stores result in `self`.
+ * Returns `self`. */
+inline SCN_Vertex* vec_vector_crossp(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+    self->x = a->y*b->z - a->z*b->y;
+    self->y = a->z*b->x - a->x*b->z;
+    self->z = a->x*b->y - a->y*b->x;
+    return self;
 }
 
-/* Calculates dotproduct of two vectors. */
-inline float vec_dotproduct(SCN_Vertex *v1, SCN_Vertex *v2) {
-    return v1->x*v2->x + v1->y*v2->y + v1->z*v2->z;
+/* Calculates dot product of vector `self` and vector `v`. Returns dot product
+ * value. */
+inline float vec_vector_dotp(SCN_Vertex *self, SCN_Vertex *v) {
+    return self->x*v->x + self->y*v->y + self->z*v->z;
 }
 
 #endif
