5d913d5cf21de5794301e319316f9fd765bc8916
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-10-24 10:03:49 +0200

    change: inline -> static inline

--- src/vectormath.h
+++ src/vectormath.h
@@ -6,7 +6,7 @@
 
 /* Copies given vector `self` and stores its copy in vector pointed by `dest`.
  * Returns `dest`. */
-inline SCN_Vertex* vec_vector_copy(SCN_Vertex *self, SCN_Vertex *dest) {
+static inline SCN_Vertex* vec_vector_copy(SCN_Vertex *self, SCN_Vertex *dest) {
     dest->x = self->x;
     dest->y = self->y;
     dest->z = self->z;
@@ -16,7 +16,7 @@ inline SCN_Vertex* vec_vector_copy(SCN_Vertex *self, SCN_Vertex *dest) {
 /* Subtracts vector `b` from vector `a` and stores result in vector `self`.
  * When `a` and `b` are interpreted as points, not vectors, this function will
  * calculate vector from point `b` towards point `a`.  Returns `self`. */
-inline SCN_Vertex* vec_vector_sub(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+static inline SCN_Vertex* vec_vector_sub(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
     self->x = a->x - b->x;
     self->y = a->y - b->y;
     self->z = a->z - b->z;
@@ -25,23 +25,23 @@ inline SCN_Vertex* vec_vector_sub(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b
 
 /* Alias for `vec_vector_sub`. Used to calculate vector from point `a` towards
  * point `b`. */
-inline SCN_Vertex* vec_vector_make(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+static inline SCN_Vertex* vec_vector_make(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
     return vec_vector_sub(self, b, a);
 }
 
 /* Returns length of given vector `self`. */
-inline float vec_vector_length(SCN_Vertex *self) {
+static inline float vec_vector_length(SCN_Vertex *self) {
     return sqrt(self->x*self->x + self->y*self->y + self->z*self->z);
 }
 
 /* Calculates Euclidean distance between given vector `self` and vector `v`. */
-inline float vec_vector_distance(SCN_Vertex *self, SCN_Vertex *v) {
+static inline float vec_vector_distance(SCN_Vertex *self, SCN_Vertex *v) {
     float dx=self->x-v->x, dy=self->y-v->y, dz=self->z-v->z;
     return sqrt(dx*dx + dy*dy + dz*dz);
 }
 
 /* Normalizes given vector "inplace". Returns `self`. */
-inline SCN_Vertex* vec_vector_normalize(SCN_Vertex *self) {
+static inline SCN_Vertex* vec_vector_normalize(SCN_Vertex *self) {
     float len=sqrt(self->x*self->x + self->y*self->y + self->z*self->z);
     self->x /= len;
     self->y /= len;
@@ -51,7 +51,7 @@ inline SCN_Vertex* vec_vector_normalize(SCN_Vertex *self) {
 
 /* Multiplicates vector `v` by scalar `t` and stores result in `self`. Returns
  * `self`. */
-inline SCN_Vertex* vec_vector_mul(SCN_Vertex *self, SCN_Vertex *v, float t) {
+static inline SCN_Vertex* vec_vector_mul(SCN_Vertex *self, SCN_Vertex *v, float t) {
     self->x = v->x * t;
     self->y = v->y * t;
     self->z = v->z * t;
@@ -59,7 +59,7 @@ inline SCN_Vertex* vec_vector_mul(SCN_Vertex *self, SCN_Vertex *v, float t) {
 }
 
 /* Adds vector `b` to vector `a` and stores result in `self`. Returns `self`. */
-inline SCN_Vertex* vec_vector_add(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+static inline SCN_Vertex* vec_vector_add(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
     self->x = a->x + b->x;
     self->y = a->y + b->y;
     self->z = a->z + b->z;
@@ -68,7 +68,7 @@ inline SCN_Vertex* vec_vector_add(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b
 
 /* Calculates cross product of vectors `a` and `b` and stores result in `self`.
  * Returns `self`. */
-inline SCN_Vertex* vec_vector_crossp(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+static inline SCN_Vertex* vec_vector_crossp(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
     self->x = a->y*b->z - a->z*b->y;
     self->y = a->z*b->x - a->x*b->z;
     self->z = a->x*b->y - a->y*b->x;
@@ -77,12 +77,12 @@ inline SCN_Vertex* vec_vector_crossp(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex
 
 /* Calculates dot product of vector `self` and vector `v`. Returns dot product
  * value. */
-inline float vec_vector_dotp(SCN_Vertex *self, SCN_Vertex *v) {
+static inline float vec_vector_dotp(SCN_Vertex *self, SCN_Vertex *v) {
     return self->x*v->x + self->y*v->y + self->z*v->z;
 }
 
 /* Calculates normalized ray vector pointing from `a` towards `b`. */
-inline SCN_Vertex* vec_vector_ray(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
+static inline SCN_Vertex* vec_vector_ray(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b) {
     self->x = b->x - a->x;
     self->y = b->y - a->y;
     self->z = b->z - a->z;
@@ -96,7 +96,7 @@ inline SCN_Vertex* vec_vector_ray(SCN_Vertex *self, SCN_Vertex *a, SCN_Vertex *b
  @param: o: ray origin
  @param: r: ray direction vector (must be normalized)
  @param: d: distance parameter */
-inline SCN_Vertex* vec_vector_raypoint(SCN_Vertex *self, SCN_Vertex *o, SCN_Vertex *r, float d) {
+static inline SCN_Vertex* vec_vector_raypoint(SCN_Vertex *self, SCN_Vertex *o, SCN_Vertex *r, float d) {
     self->x = o->x + d*r->x;
     self->y = o->y + d*r->y;
     self->z = o->z + d*r->z;
