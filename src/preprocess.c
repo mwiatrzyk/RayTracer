b8e7c350c6022066b5eec5ffa165ad9c90e2bd32
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-11-05 00:37:11 +0100

    add `preprocess` module

---
+++ src/preprocess.c
@@ -0,0 +1,3 @@
+#include "preprocess.h"
+
+// vim: tabstop=2 shiftwidth=2 softtabstop=2
---
+++ src/preprocess.h
@@ -0,0 +1,6 @@
+#ifndef __PREPROCESS_H
+#define __PREPROCESS_H
+
+#endif
+
+// vim: tabstop=2 shiftwidth=2 softtabstop=2
