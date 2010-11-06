0e1b21d646d1856ddf44028779069578048d9c57
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2010-11-06 22:28:05 +0100

    first version of voxelization procedure

--- src/voxelize.c
+++ src/voxelize.c
@@ -1,3 +1,157 @@
 #include "voxelize.h"
+#include "vectormath.h"
+#include "error.h"
+#include "common.h"
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+///////////////////////////////////////////////////////////////
+RT_Udd* rtUddCreate(RT_Scene* scene) {
+  int k;
+  float ds[3], v, tmp;
+  RT_Udd *res=NULL;
+
+  // create result object
+  res = malloc(sizeof(RT_Udd));
+  if(!res) {
+    errno = E_MEMORY;
+    return NULL;
+  }
+
+  // calculate domain size
+  for(k=0; k<3; k++) {
+    ds[k] = scene->dmax[k] - scene->dmin[k];
+  }
+  RT_DEBUG("domain size: x=%.3f, y=%.3f, z=%.3f", ds[0], ds[1], ds[2]);
+  
+  // calculate grid size and size of single element of grid
+  v = pow(scene->nt/(ds[0]*ds[1]*ds[2]), 0.33333f);
+  for(k=0; k<3; k++) {
+    tmp = ceil(ds[k]*v);  // number of grid elements in k-direction
+    res->nv[k] = tmp;
+    res->s[k] = ds[k]/tmp;  // size of voxel in k-direction
+  }
+  RT_DEBUG("number of voxels: i=%d, j=%d, k=%d", res->nv[0], res->nv[1], res->nv[2]);
+  RT_DEBUG("total number of voxels: %d", res->nv[0]*res->nv[1]*res->nv[2]);
+  RT_DEBUG("total number of triangles: %d", scene->nt);
+  RT_DEBUG("size of single voxel: i=%.3f, j=%.3f, k=%.3f", res->s[0], res->s[1], res->s[2]);
+
+  // create voxel grid array
+  tmp = res->nv[0] * res->nv[1] * res->nv[2];
+  res->v = malloc(tmp*sizeof(RT_Voxel));
+  if(!res->v) {
+    free(res);
+    errno = E_MEMORY;
+    return NULL;
+  }
+  memset(res->v, 0, tmp*sizeof(RT_Voxel));
+
+  return res;
+}
+///////////////////////////////////////////////////////////////
+void rtUddDestroy(RT_Udd **self) {
+  RT_Udd *ptr = *self;
+  if(ptr->v) {
+    free(ptr->v);
+  }
+  free(ptr);
+  *self = NULL;
+}
+///////////////////////////////////////////////////////////////
+void rtUddVoxelize(RT_Udd *self, RT_Scene *scene) {
+  int32_t i, j, k;
+  int32_t ni=30, nj=30, nk=30;//self->nv[0], nj=self->nv[1], nk=self->nv[2];
+  int32_t itmp=0;
+  float s1, s2;
+  RT_Triangle *t=scene->t, **tmp;
+
+  // make temporary storage for assigned triangles
+  tmp = malloc(scene->nt*sizeof(RT_Triangle*));
+  if(!tmp) {
+    errno = E_MEMORY;
+    return;
+  }
+  memset(tmp, 0, scene->nt*sizeof(RT_Triangle*));
+
+  // loop through grid array
+  for(i=0; i<ni; i++) {
+    for(j=0; j<nj; j++) {
+      for(k=0; k<nk; k++) {
+
+        /* Triangle is included in the voxel if at least one of following
+         * alternatives is true:
+         * 1) At least one of its vertices is inside voxel
+         * 2) At least one of triangle segments intersects voxel
+         * 3) Triangle plane intersects voxel and voxel is inside triangle */
+        float x1 = scene->dmin[0] + i*self->s[0];
+        float x2 = x1 + self->s[0];
+        float y1 = scene->dmin[1] + j*self->s[1];
+        float y2 = y1 + self->s[1];
+        float z1 = scene->dmin[2] + k*self->s[2];
+        float z2 = z1 + self->s[2];
+
+        /* Vertices of current voxel can now be calculated as follows:
+         * 1) Bottom vertices:
+         *    (x1,y1,z1) - close left
+         *    (x2,y1,z1) - close right
+         *    (x1,y1,z2) - far left
+         *    (x2,y1,z2) - far right 
+         * 2) Upper vertices:
+         *    Like above, but with y2 instead of y1. */
+        RT_Vertex4f bcl={x1, y1, z1, 0.0f}, bcr={x2, y1, z1, 0.0f};
+        RT_Vertex4f bfl={x1, y1, x2, 0.0f}, bfr={x2, y1, z2, 0.0f};
+        RT_Vertex4f ucl={x1, y2, z1, 0.0f}, ucr={x2, y2, z1, 0.0f};
+        RT_Vertex4f ufl={x1, y2, x2, 0.0f}, ufr={x2, y2, z2, 0.0f};
+        
+        // initialize buffer indexer
+        itmp = 0;
+
+        // loop through array of triangles to assign triangles for current voxel
+        RT_Triangle *t=scene->t, *maxt=(RT_Triangle*)(scene->t + scene->nt);
+        while(t < maxt) {
+          /* Apply voxel's corner nodes to triangle's plane equation and check
+           * signs of result. If at least one corner have different sign that
+           * any other then triangle's plane intersects voxel. */
+          s1 = rtVectorDotp(t->n, bcl) + t->d;
+          s2 = rtVectorDotp(t->n, bcr) + t->d;
+          if(s1*s2 > 0.0f) {  // if sign for both vertices is the same
+            s2 = rtVectorDotp(t->n, bfl) + t->d;
+            if(s1*s2 > 0.0f) {
+              s2 = rtVectorDotp(t->n, bfr) + t->d;
+              if(s1*s2 > 0.0f) {
+                s2 = rtVectorDotp(t->n, ucl) + t->d;
+                if(s1*s2 > 0.0f) {
+                  s2 = rtVectorDotp(t->n, ucr) + t->d;
+                  if(s1*s2 > 0.0f) {
+                    s2 = rtVectorDotp(t->n, ufl) + t->d;
+                    if(s1*s2 > 0.0f) {
+                      s2 = rtVectorDotp(t->n, ufr) + t->d;
+                      if(s1*s2 > 0.0f) {
+                        /* If we are here, all corners of voxel are on the same
+                         * side of triangle's plane. That means that current
+                         * triangle is not added to vertex. */
+                        t++;
+                        continue;
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          
+          // add triangle pointer to buffer
+          tmp[itmp++] = t;
+
+          // proceed to next triangle
+          t++;
+        }
+      }
+    }
+  }
+}
 
 // vim: tabstop=2 shiftwidth=2 softtabstop=2
--- src/voxelize.h
+++ src/voxelize.h
@@ -1,8 +1,8 @@
 /*
   Module that performs uniform domain division of scene.
 */
-#ifndef __DOMDIVISION_H
-#define __DOMDIVISION_H
+#ifndef __VOXELIZE_H
+#define __VOXELIZE_H
 
 #include "scene.h"
 
@@ -18,17 +18,34 @@ typedef struct _RT_Voxel {
 /* Structure that groups all voxels in one place. "UDD" stands for "Uniform
  * Domain Division". */
 typedef struct _RT_Udd {
-  float si, sj, sk;     // size of single voxel (x, y, z)
-  int32_t ni, nj, nk;   // size of voxel grid in x, y and z directions
-  int32_t nv;           // number of voxels in `v` array
-  RT_Voxel *v;          // array of voxels mapped from 3D array to 1D array
+  float s[3];     // size of single voxel (x, y, z)
+  int32_t nv[3];  // voxel grid size (nv[0]*nv[1]*nv[2] is number of items in `v` array)
+  RT_Voxel *v;    // array of voxels mapped from 3D array to 1D array
 } RT_Udd;
+
+
+//// INLINE FUNCTIONS /////////////////////////////////////////
+
+/* Calculates mapping from 3D coords into 1D array offset value. Used to get 1D
+ * index of voxel at position (i,j,k).
+
+:param: s: pointer to RT_Udd object
+:param: i, j, k: voxel coordinates */
+static inline int32_t rtVoxelArrayOffset(const RT_Udd *s, int32_t i, int32_t j, int32_t k) {
+  return (i*s->nv[1] + j)*s->nv[2] + k;
+}
 
 
 //// FUNCTIONS ////////////////////////////////////////////////
 
 /* Initializes Uniform Domain Division structure. */
-RT_Udd* rtUddCreate(RT_Scene* scene);
+RT_Udd* rtUddCreate(RT_Scene *scene);
+
+/* Releases memory occupied by RT_Udd object. */
+void rtUddDestroy(RT_Udd **self);
+
+/* Performs scene voxelization (fills voxels with triangles). */
+void rtUddVoxelize(RT_Udd *self, RT_Scene *scene);
 
 #endif
 
